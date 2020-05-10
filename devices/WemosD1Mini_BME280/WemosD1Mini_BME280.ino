282e2d421fdf46372a994c230a7e70aa4e459826
Ilya Malikov <iammalikov@protonmail.com>
2020-05-10 23:57:14 +0300

    Added device (Wemos D1 mini + BME280) sketch

---
+++ devices/WemosD1Mini_BME280/WemosD1Mini_BME280.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+HTTPClient http;
+WiFiClient client;
+Adafruit_BME280 bme; // I2C
+
+const char* ssid = "DIR-615T-1666";
+const char* password = "13234003";
+const String host = "https://api.thingspeak.com/update?api_key=";
+const String apiKey = "1DPM55BZC50VTL3M";
+const unsigned int frequencySendingData = 15000;
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial); // time to get serial running
+
+  // Connecting to WiFi network
+  WiFi.mode(WIFI_STA); // Set ESP8266 to be a WiFi-client
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialization and availability check BME280
+  const boolean sensorStatus = bme.begin();
+
+  if (!sensorStatus) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+
+    while (1) delay(1000);
+  }
+
+  Serial.println("");
+  Serial.println("BME280 connected");
+}
+
+
+void loop() {
+  float temperature = bme.readTemperature(); // °C
+  float humidity = bme.readHumidity(); // %
+  float pressure = bme.readPressure() / 100.0F; // hPa
+
+  sendData(temperature, humidity, pressure);
+
+  delay(frequencySendingData);
+}
+
+void sendData(float temperature, float humidity, float pressure) {
+  String temperatureParameter = "&field1=" + String(temperature);
+  String humidityParameter = "&field2=" + String(humidity);
+  String pressureParameter = "&field3=" + String(pressure);
+
+  String url = host + apiKey + temperatureParameter + humidityParameter + pressureParameter;
+
+  Serial.print("connecting to ");
+  Serial.println(host + apiKey);
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  http.begin(url);
+
+  int httpCode = http.GET();
+
+  Serial.print("HTTP CODE = ");
+  Serial.println(httpCode);
+  Serial.println();
+
+  if (httpCode) {
+    if (httpCode == 200) {
+      String payload = http.getString();
+      Serial.println("Response: ");
+      Serial.println(payload);
+    }
+  }
+  
+  Serial.println("closing connection");
+  http.end();
+}
